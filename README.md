7f9aa7911f3f0a82ff5890f6d4fb916025961a78
hasan <ocakhasan99@gmail.com>
2020-08-29 18:14:30 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# movie-recommender
+
+## Dataset
+This simple recommendation uses the dataset from the [TMDB 500 Movie Dataset](https://www.kaggle.com/tmdb/tmdb-movie-metadata). 
+
+## How it works
+
+To make it work first you need to run the `main.py` file to create `cosine.pickle` file.
+
+This operation will create `cosine.pickle` file which we basically use for the similarities of text.
+
+After that, you need to run these commands
+```
+set FLASK_APP=app (Windows)
+export FLASK_APP=app (Linux, MacOS)
+```
+
+After that,
+```
+flask run
+```
+It will run the website in http://127.0.0.1:5000/.
+
+It recommends movie by based on their overviews.
+It looks for text similarity and shows the most similar 10 results for the given input.
+
+## Screenshots
+
+![First Scrrenshot](https://github.com/ocakhasan/movie-recommender/blob/master/screenshots/first.png)
+
+I am planning to deploy in soon after doing some modifications.
---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, request, redirect, flash, url_for
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+app = Flask(__name__)
+
+import pandas as pd
+import pickle
+
+df = pd.read_csv("data.csv")
+
+df['overview'] = df['overview'].fillna('')
+
+vectorizer = TfidfVectorizer(stop_words="english")
+
+
+tf_idf_mat = vectorizer.fit_transform(df['overview'])
+
+cosine_sim = linear_kernel(tf_idf_mat, tf_idf_mat)
+
+titles = pd.Series(df.index, index=df['title']).drop_duplicates()
+
+print(titles)
+
+
+
+def get_recommendations(movie_title, cosine_similarity = cosine_sim):
+    index_movie = titles[movie_title]
+    
+    similarities = cosine_similarity[index_movie]
+    
+    similarity_scores = list(enumerate(similarities))
+    
+    similarity_scores = sorted(similarity_scores , key=lambda x: x[1], reverse = True)
+    
+    similarity_scores = similarity_scores[1:11]
+    
+    similar_indexes = [x[0] for x in similarity_scores]
+    
+    return df.iloc[similar_indexes]
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    length = 0
+    text=""
+    context = {'movies': [],
+                'urls' : [],
+                'release_dates': [],
+                'runtimes':[],
+                'overviews': []}
+    
+    if request.method=="POST":
+        text = request.form['fname']
+        try:
+            recommended_df = get_recommendations(text)
+            context['movies'] = recommended_df.title.values
+            context['urls'] = recommended_df.homepage.values
+            context['release_dates'] = recommended_df.release_date.values
+            context['runtimes'] = recommended_df.runtime.values
+            context['overviews'] = recommended_df.overview.values
+
+            length = len(context['movies'])
+            print(text)
+        except:
+            return render_template('index.html', error=True)
+    
+    return render_template('index.html', length=length, context=context, movie_name=text, error=False)
+
+if __name__ == '__main__':
+    app.run()
